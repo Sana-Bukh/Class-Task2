26fa0c78bf06c3cb095c08e30ddd32341d9f6613
Maryam5676 <maryamnasir2942004@gmail.com>
2024-02-23 20:50:11 +0500

    add file

---
+++ index.js
@@ -0,0 +1,34 @@
+// calculating total cost
+// function calculateTotalCost(price, quantity) {
+//     return price * quantity;
+// }
+// console.log( calculateTotalCost(10, 5));
+// console.log( calculateTotalCost(20, 4));
+// console.log( calculateTotalCost(30, 3));
+
+
+//  area of rectangle
+// function calculateArea(length, width) {
+//     return length * width;
+// }
+// console.log( calculateArea(4, 6));
+// console.log( calculateArea(9, 8));
+// console.log( calculateArea(7, 10));
+
+
+//  to calculate BMI
+// function calculateBMI(weight, height) {
+//     return weight / (height * height);
+// }
+// console.log( calculateBMI(70, 1.75));
+// console.log( calculateBMI(44, 1.55));
+// console.log( calculateBMI(55, 1.66));
+
+
+//  to calculate area of triangle
+function calculateTriangleArea(base, height) {
+    return (base * height) / 2;
+}
+console.log( calculateTriangleArea(  5 , 8)); 
+console.log( calculateTriangleArea(6, 7)); 
+console.log( calculateTriangleArea(4, 3));
